90ebb9e4855b14e0087fd39d840a8efb3f32655e
rkig0111 <kliglich.romuald@free.fr>
2023-08-17 17:31:28 +0200

    parametrage templates

--- imagerie/views.py
+++ imagerie/views.py
@@ -1,3 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Vlan
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def list_vlan(request):
+    try:
+        vlans = Vlan.objects.all()
+    except Vlan.DoesNotExist:
+        raise Http404("Vlan does not exist")
+    print("vlans ---->  ", vlans)
+    return render(request, "imagerie/list_vlan.html", {"vlans": vlans})
--- modalite_bio/urls.py
+++ modalite_bio/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from imagerie import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("imagerie/", include("imagerie.urls")),
+    path('', views.index, name="index"),
+    path('vlan', views.list_vlan, name='list_vlan')
 ]
